abe9630351e6b892d09f07b58c8c78ba77f3ed29
NathyOne <nathanielabebe32@gmail.com>
2025-09-09 16:27:50 +0300

    changed query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,3 @@
-SELECT * FROM Booking INNER JOIN user ON Booking.host_id = user.user_id;
-SELECT * FROM Property LEFT JOIN Review ON Property.property_id= Review.property_id;
+SELECT * FROM Booking INNER JOIN Users ON Booking.host_id = user.user_id;
+SELECT * FROM Property LEFT JOIN Review ON Property.property_id = Review.property_id ORDER BY Property.property_id;;
 SELECT * FROM Users FULL OUTER JOIN Booking ON Users.user_id = Booking.user_id;
