092088f4d135f2fc99d6ca73f450344aba0f18c5
NathyOne <nathanielabebe32@gmail.com>
2025-09-09 16:19:25 +0300

    added oins_queries.sql, README.md

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,3 @@
+SELECT * FROM Booking INNER JOIN user ON Booking.host_id = user.user_id;
+SELECT * FROM Property LEFT JOIN Review ON Property.property_id= Review.property_id;
+SELECT * FROM Users FULL OUTER JOIN Booking ON Users.user_id = Booking.user_id;
