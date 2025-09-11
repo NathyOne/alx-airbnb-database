f91d8a675647c38e78df0c948b904f8cb5956443
NathyOne <nathanielabebe32@gmail.com>
2025-09-11 08:00:40 +0300

    adding correlated and non corelated queries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,2 +1,2 @@
-SELECT * FROM Property WHERE Rating >(SELECT AVG(rating) FROM Property);
+SELECT * FROM Property WHERE Rating >(SELECT AVG(rating) FROM Property)>4;
 SELECT * FROM Users u WHERE ( SELECT COUNT(*) FROM Booking b WHERE b.user_id = u.user_id)>3;
