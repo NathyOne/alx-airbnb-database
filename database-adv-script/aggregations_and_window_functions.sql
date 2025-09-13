e34ac0b19b2eb91a122ad531dc108d4e2275c3e0
NathyOne <nathanielabebe32@gmail.com>
2025-09-13 21:26:12 +0300

    index added

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,4 +2,4 @@ SELECT * FROM COUNT(*) AS total_count FROM Booking GROUP BY user_id;
 
 
 #Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
-SELECT ROW_NUMBER OVER() AS RANK FROM Properties COUNT(*)
+SELECT ROW_NUMBER OVER(SELECT COUNT()) AS RANK FROM Properties COUNT(*)
---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,9 @@
+CREATE INDEX idx_property_id ON Property (property_id);
+
+CREATE INDEX idx_booking_id ON Booking (booking_id);
+
+CREATE INDEX idx_payment_id ON Payment (payment_id);
+
+CREATE INDEX idx_review_id ON Review (review_id);
+
+CREATE INDEX idx_message_id ON Message (message_id);
