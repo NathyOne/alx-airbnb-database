fe3f5797bf31ebdb91bc447a23eebc19e07a1df2
NathyOne <nathanielabebe32@gmail.com>
2025-09-15 23:23:34 +0300

    correction on both files

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,16 @@
 #Write an initial query that retrieves all bookings along with the user details, property details, and payment details and save it on
-SELECT b.booking_id, u.first_name,u.last_name, p.propery_name p.property_id, pay.payment_id, pay.amount FROM Booking b  INNER JOIN  Users u ON b.user_id = u.user_id
-INNER JOIN  p Property ON b.property_id = p.property_id
-LEFT JOIN pay payment ON b.payment_id = pay.payment_id;
+SELECT 
+    b.booking_id,
+    u.first_name,
+    u.last_name,
+    p.property_name,
+    p.property_id,
+    pay.payment_id,
+    pay.amount
+FROM 
+    Booking b
+    INNER JOIN Users u ON b.user_id = u.user_id
+    INNER JOIN Property p ON b.property_id = p.property_id
+    LEFT JOIN payment pay ON b.payment_id = pay.payment_id
+ORDER BY 
+    b.booking_id DESC;
