936f10d7d73622db8ebec2d40690e530aa0e9159
NathyOne <nathanielabebe32@gmail.com>
2025-09-16 17:23:48 +0300

    performance.sql change

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,7 +1,11 @@
-SELECT b.booking_id, b.booking_date, u.first_name, u.last_name, u.email, p.property_name, p.property_id, pay.payment_id, pay.amount, pay.payment_date
+SELECT b.booking_id, u.first_name, u.last_name, p.property_name, p.property_id, pay.payment_id, pay.amount
 FROM
     Booking b
     INNER JOIN Users u ON b.user_id = u.user_id
     INNER JOIN Property p ON b.property_id = p.property_id
-    INNER JOIN payment pay ON b.payment_id = pay.payment_id
-ORDER BY b.booking_date DESC;
+    LEFT JOIN payment pay ON b.payment_id = pay.payment_id
+WHERE
+    b.status = 'confirmed'
+    AND b.booking_date >= '2025-01-01'
+    AND b.booking_date < '2025-12-31'
+ORDER BY b.booking_id DESC;
