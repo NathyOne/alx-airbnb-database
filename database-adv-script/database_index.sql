788cf7be49aacda4e8843fe707dfa52a81c7a854
NathyOne <nathanielabebe32@gmail.com>
2025-09-18 15:19:14 +0300

    improvement

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -7,3 +7,13 @@ CREATE INDEX idx_payment_id ON Payment (payment_id);
 CREATE INDEX idx_review_id ON Review (review_id);
 
 CREATE INDEX idx_message_id ON Message (message_id);
+
+EXPLAIN CREATE INDEX idx_property_id ON Property (property_id);
+
+CREATE INDEX idx_booking_id ON Booking (booking_id);
+
+CREATE INDEX idx_payment_id ON Payment (payment_id);
+
+CREATE INDEX idx_review_id ON Review (review_id);
+
+CREATE INDEX idx_message_id ON Message (message_id);
