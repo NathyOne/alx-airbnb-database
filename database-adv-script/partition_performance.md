1c70c43bbbb259eb113ed8129582f2f103391259
NathyOne <nathanielabebe32@gmail.com>
2025-09-18 15:04:22 +0300

    partition on booking

---
+++ database-adv-script/partition_performance.md
@@ -0,0 +1,1 @@
+# table partioning to increase performance of queries
---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,12 @@
+# ssume the Booking table is large and query performance is slow. Implement partitioning on the Booking table based on the start_date column. Save the query in a file partitioning.sql
+
+#Test the performance of queries on the partitioned table (e.g., fetching bookings by date range).
+
+#Write a brief report on the improvements you observed.
+
+PARTITION BY RANGE (YEAR(start_date))(
+PARTITION P0 VALUES LESS THAN(2019),
+PARTITION P0 VALUES LESS THAN(2020),
+PARTITION P0 VALUES LESS THAN(2021),
+PARTITION P0 VALUES LESS THAN(2022)
+);
