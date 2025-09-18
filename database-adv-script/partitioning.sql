e88abe3c9b413fcb9b623eafc1c0ccfe3dba11cf
NathyOne <nathanielabebe32@gmail.com>
2025-09-18 15:12:52 +0300

    explain function added

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,5 +1,21 @@
+ALTER TABLE Booking
+PARTITION BY
+    RANGE (YEAR(start_date)) (
+        PARTITION P0
+        VALUES
+            LESS THAN (2019),
+            PARTITION P1
+        VALUES
+            LESS THAN (2020),
+            PARTITION P2
+        VALUES
+            LESS THAN (2021),
+            PARTITION P3
+        VALUES
+            LESS THAN (2022)
+    );
 
-
+EXPLAIN
 ALTER TABLE Booking
 PARTITION BY
     RANGE (YEAR(start_date)) (
