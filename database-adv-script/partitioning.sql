f8aacb9d4de2a54df9ef727ed61996006cb23113
NathyOne <nathanielabebe32@gmail.com>
2025-09-18 15:17:08 +0300

    error fixing

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,4 +1,15 @@
-ALTER TABLE Booking
+CREATE TABLE Booking (
+    booking_id INT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status booking_status NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property (property_id),
+    FOREIGN KEY (user_id) REFERENCES Users (user_id)
+)
 PARTITION BY
     RANGE (YEAR(start_date)) (
         PARTITION P0
